5e24239e81b8565672d95379f808636cf83304a7
Andrew Tonascia <atonasci@ucsd.edu>
2016-04-18 19:19:03 -0700

    first sum commit

--- app/src/main/java/com/example/andrew/lab30/MainActivity.java
+++ app/src/main/java/com/example/andrew/lab30/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.andrew.lab30;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText editText1 = (EditText)findViewById(R.id.edit_text1);
+        final EditText editText2 = (EditText)findViewById(R.id.edit_text2);
+        final TextView textView = (TextView)findViewById(R.id.tv1);
+        Button button = (Button)findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int num1 = Integer.parseInt(editText1.getText().toString());
+                int num2 = Integer.parseInt(editText2.getText().toString());
+                int sum = num1 + num2;
+                textView.setText(""+sum);
+                editText1.setText("");
+                editText2.setText("");
+            }
+        });
     }
 }
